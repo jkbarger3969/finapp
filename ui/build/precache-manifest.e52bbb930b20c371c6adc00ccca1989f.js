be0fdfcb6444113c15f74299703734aca16e4d0a
Michael Price <mikepricedev@gmail.com>
2020-01-28 20:07:31 -0600

    adds build to repo

---
+++ ui/build/precache-manifest.e52bbb930b20c371c6adc00ccca1989f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b13c108194074147eab3bd6a12c73f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "222399e27ded65ba9fd9",
+    "url": "/static/js/2.2aed8fbf.chunk.js"
+  },
+  {
+    "revision": "666053a613c05f868511",
+    "url": "/static/js/main.e81a0665.chunk.js"
+  },
+  {
+    "revision": "a5136e3714b86c6f3cbf",
+    "url": "/static/js/runtime-main.27b6c454.js"
+  }
+]);
