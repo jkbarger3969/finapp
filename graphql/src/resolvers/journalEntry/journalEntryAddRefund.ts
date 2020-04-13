873ce1d3306efdafed8990218ea3ddfb2e1b2e37
Michael Price <mikepricedev@gmail.com>
2020-04-12 19:33:12 -0500

    Parallelizes ops.

--- graphql/src/resolvers/journalEntry/journalEntryAddRefund.ts
+++ graphql/src/resolvers/journalEntry/journalEntryAddRefund.ts
@@ -29,49 +29,58 @@ const journalEntryAddRefund: MutationResolvers["journalEntryAddRefund"] = async 
     throw new Error("Refund total must be greater than 0.");
   }
 
-  const { db, user } = context;
-
-  const collection = db.collection("journalEntries");
-
-  const srcEntryId = new ObjectID(id);
-
-  // Ensure source entry exists and get entry and refund totals.
-  const [srcEntryState] = (await collection
-    .aggregate([
-      { $match: { _id: srcEntryId } },
-      stages.entryTotal,
-      stages.refundTotal,
-      {
-        $project: {
-          entryTotal: true,
-          refundTotal: true,
-        },
-      },
-    ])
-    .toArray()) as [{ entryTotal: number; refundTotal: number }];
-
-  if (!srcEntryState) {
-    throw new Error(`Journal Entry "${id}" does not exist.`);
-  }
-
-  const { entryTotal, refundTotal } = srcEntryState;
-
-  // Ensure aggregate refunds do NOT exceed the original transaction amount
-  if (entryTotal < refundTotal + totalDecimal) {
-    throw new Error(
-      "Refunds cannot total more than original transaction amount."
-    );
-  }
-
   const date = moment(dateStr, moment.ISO_8601);
   if (!date.isValid()) {
     throw new Error(`Date "${dateStr}" not a valid ISO 8601 date string.`);
   }
 
+  const { db, user } = context;
+
   const docHistory = new DocHistory({ node: userNodeType, id: user.id });
 
+  const collection = db.collection("journalEntries");
+
+  const srcEntryId = new ObjectID(id);
+
+  let refundEntryId: ObjectID;
+
+  await Promise.all([
+    (async () => {
+      // Ensure source entry exists and get entry and refund totals.
+      const [srcEntryState] = (await collection
+        .aggregate([
+          { $match: { _id: srcEntryId } },
+          stages.entryTotal,
+          stages.refundTotal,
+          {
+            $project: {
+              entryTotal: true,
+              refundTotal: true,
+            },
+          },
+        ])
+        .toArray()) as [{ entryTotal: number; refundTotal: number }];
+
+      if (!srcEntryState) {
+        throw new Error(`Journal Entry "${id}" does not exist.`);
+      }
+
+      const { entryTotal, refundTotal } = srcEntryState;
+
+      // Ensure aggregate refunds do NOT exceed the original transaction amount
+      if (entryTotal < refundTotal + totalDecimal) {
+        throw new Error(
+          "Refunds cannot total more than original transaction amount."
+        );
+      }
+    })(),
+    (async () => {
+      refundEntryId = await getUniqueId("refund.id", collection);
+    })(),
+  ]);
+
   const refundEntry = {
-    id: await getUniqueId("refund.id", collection),
+    id: refundEntryId,
     date: docHistory.addValue(date.toDate()),
     description: description ? docHistory.addValue(description) : [],
     total: docHistory.addValue(total),
