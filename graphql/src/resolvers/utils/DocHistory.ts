f22299316a1210b65ac11db310f4a0f54f633810
Michael Price <mikepricedev@gmail.com>
2020-04-18 21:13:09 -0500

    Reworks DocHistory adding new classes to handle historicl doc creation and updates

--- graphql/src/resolvers/utils/DocHistory.ts
+++ graphql/src/resolvers/utils/DocHistory.ts
@@ -19,72 +19,161 @@ export interface HistoryObject<T> {
   readonly createdOn: Date;
 }
 
-export interface RootHistoryObject {
+export interface RootHistory {
   readonly lastUpdate: Date;
   readonly createdOn: Date;
   readonly createdBy: CreatedBy;
 }
 
-export interface UpdateValuePushArg<T> {
+export interface HistoricalDoc {
+  [field: string]: [HistoryObject<any>];
+}
+
+export type HistoricalRootDoc = RootHistory & HistoricalDoc;
+
+export interface UpdateValue<T> {
   readonly $each: [HistoryObject<T>];
   readonly $position: 0;
 }
 
-export type UpdateValuePush = {
-  [field: string]: UpdateValuePushArg<any>;
+export type UpdatePush = {
+  [field: string]: UpdateValue<any>;
 };
 
-export default class DocHistory {
-  private readonly _push_: UpdateValuePush = {};
-  private _hasUpdate_ = false;
+export type Update = {
+  $push: UpdatePush;
+  $set: { readonly lastUpdate: Date } | { [lastUpdateField: string]: Date };
+};
 
+class NewHistoricalDoc<T extends boolean> {
+  private readonly _doc_: T extends true ? HistoricalRootDoc : HistoricalDoc;
+  constructor(private readonly _docHistory_: DocHistory, withRootHistory: T) {
+    this._doc_ = (withRootHistory ? { ..._docHistory_.update } : {}) as any;
+  }
+  addField<T>(field: string, value: T): this {
+    (this._doc_ as any)[field] = this._docHistory_.newValue(value);
+    return this;
+  }
+  doc() {
+    return this._doc_;
+  }
+}
+
+class UpdateHistoricalDoc {
+  private readonly _update_: Update;
+  private readonly _prependUpdateFields_: string = "";
+  private _hasUpdate_ = false;
+  constructor(
+    private readonly _docHistory_: DocHistory,
+    args?:
+      | {
+          prependUpdateFields?: string;
+          prependLastUpdate?: string;
+        }
+      | string
+  ) {
+    if (args) {
+      if (typeof args === "string") {
+        this._prependUpdateFields_ = `${args}.`;
+        this._update_ = {
+          $push: {},
+          $set: {
+            [`${this._prependUpdateFields_}lastUpdate`]: _docHistory_.date,
+          },
+        };
+      } else {
+        if (args.prependUpdateFields) {
+          this._prependUpdateFields_ = `${args.prependUpdateFields}.`;
+        }
+        if (args.prependLastUpdate) {
+          this._update_ = {
+            $push: {},
+            $set: {
+              [`${args.prependLastUpdate}.lastUpdate`]: _docHistory_.date,
+            },
+          };
+        }
+      }
+    } else {
+      this._update_ = {
+        $push: {},
+        $set: { lastUpdate: _docHistory_.date },
+      };
+    }
+  }
+  get hasUpdate() {
+    return this._hasUpdate_;
+  }
+  updateField<T>(field: string, value: T): this {
+    this._update_.$push[`${this._prependUpdateFields_}${field}`] = {
+      $each: [this._docHistory_.historyObject(value)],
+      $position: 0,
+    };
+    this._hasUpdate_ = true;
+    return this;
+  }
+  updateFields(fieldValuesMap: Iterable<[string, any]>): this {
+    for (const [field, value] of fieldValuesMap) {
+      this.updateField(field, value);
+    }
+    return this;
+  }
+  update(): Update {
+    return this._update_;
+  }
+}
+
+export default class DocHistory {
   constructor(
     private readonly _by_: CreatedBy,
     private readonly _date_ = new Date()
   ) {}
 
-  get updatePushArg() {
-    return this._push_;
-  }
-
-  get hasUpdate() {
-    return this._hasUpdate_;
-  }
-
-  get update() {
-    return this.hasUpdate
-      ? {
-          $set: {
-            lastUpdate: this.lastUpdate,
-          },
-          $push: {
-            ...this._push_,
-          },
-        }
-      : null;
-  }
-
-  get lastUpdate() {
+  get date() {
     return this._date_;
   }
 
-  get createdBy() {
+  get by() {
     return this._by_;
   }
 
-  get rootHistoryObject(): RootHistoryObject {
+  rootHistory(): RootHistory {
     return {
       lastUpdate: this._date_,
       createdOn: this._date_,
       createdBy: this._by_,
-    } as const;
+    };
+  }
+
+  // newHistoricalDoc(withRootHistory: true): NewHistoricalDoc<true>;
+  // newHistoricalDoc(withRootHistory: false): NewHistoricalDoc<false>;
+  newHistoricalDoc<T extends boolean>(withRootHistory: T) {
+    return new NewHistoricalDoc<T>(this, withRootHistory);
+  }
+
+  updateHistoricalDoc(
+    prependFields?: ConstructorParameters<typeof UpdateHistoricalDoc>[1]
+  ): UpdateHistoricalDoc {
+    return new UpdateHistoricalDoc(this, prependFields);
+  }
+
+  historyObject<T>(value: T): HistoryObject<T> {
+    return {
+      value,
+      createdBy: this._by_,
+      createdOn: this._date_,
+    };
+  }
+
+  newValue<T>(value: T): [HistoryObject<T>] {
+    return [this.historyObject(value)];
   }
 
   getPresentValues(presentValueMap: Iterable<string>) {
     return DocHistory.getPresentValues(presentValueMap);
   }
 
-  static getAllFieldsPresentValues(
+  static getPresentValuesAllFields(
     args: { path?: string; exclude?: Iterable<string> } = {
       path: "$$ROOT",
     }
@@ -158,32 +247,5 @@ export default class DocHistory {
     }
 
     return presentValueProjection;
-  }
-
-  historyObject<T>(value: T): HistoryObject<T> {
-    return {
-      value,
-      createdBy: this._by_,
-      createdOn: this._date_,
-    };
-  }
-
-  updateValue<T>(field: string, value: T): this {
-    this._push_[field] = {
-      $each: [this.historyObject(value)],
-      $position: 0,
-    };
-    this._hasUpdate_ = true;
-    return this;
-  }
-  updateValues(fieldValuesMap: Iterable<[string, any]>): this {
-    for (const [field, value] of fieldValuesMap) {
-      this.updateValue(field, value);
-    }
-    return this;
-  }
-
-  addValue<T>(value: T): [HistoryObject<T>] {
-    return [this.historyObject(value)];
   }
 }
