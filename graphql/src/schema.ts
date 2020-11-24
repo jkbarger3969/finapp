32b80957d2ffb42f3ba8344f31777aea5218b29b
Michael Price <mikepricedev@gmail.com>
2020-11-23 21:21:15 -0600

    build: Adds @graphql-tools/merge and @graphql-tools/load-files in place   of deprecated merge-graphql-schemas.

--- graphql/src/schema.ts
+++ graphql/src/schema.ts
@@ -1,9 +1,13 @@
-import * as pathN from 'path';
-import * as mergeGraphqlSchemas from 'merge-graphql-schemas';
+import * as pathN from "path";
+// import * as mergeGraphqlSchemas from "merge-graphql-schemas";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
 
-const fileLoader = mergeGraphqlSchemas.fileLoader;
-const mergeTypes = mergeGraphqlSchemas.mergeTypes;
+// const fileLoader = mergeGraphqlSchemas.fileLoader;
+// const mergeTypes = mergeGraphqlSchemas.mergeTypes;
 
-const typesArray = fileLoader(pathN.resolve(__dirname, '../schema/**/*.gql'));
+const typesArray = loadFilesSync(
+  pathN.resolve(__dirname, "../schema/**/*.gql")
+);
 
-export default mergeTypes(typesArray, { all: true });
+export default mergeTypeDefs(typesArray);
