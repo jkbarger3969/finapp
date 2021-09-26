aeaf9f0f040848aca6134986dbfed8c1628428ec
Michael Price <mikepricedev@gmail.com>
2021-09-26 15:51:13 -0500

    fix: Path case.

--- graphql/src/resolvers/department/index.ts
+++ graphql/src/resolvers/department/index.ts
@@ -1,4 +1,4 @@
 export { department } from "./department";
 export { departments, whereDepartments } from "./departments";
-export { Department, DepartmentAncestor } from "./departmentResolvers";
-export { validateDepartment } from "./DepartmentValidators";
+export { Department, DepartmentAncestor } from "./DepartmentResolvers";
+export { validateDepartment } from "./departmentValidators";
